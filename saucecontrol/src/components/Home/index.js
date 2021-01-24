48a577a2ebe4b2ddd12b7110bc46ca0813bf65be
Alex Peterson <peter536@purdue.edu>
2021-01-24 06:08:56 -0500

    Maybe home has some recipe cards

--- saucecontrol/src/components/Home/index.js
+++ saucecontrol/src/components/Home/index.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import PreviewCard from '../PreviewCard';
+
+const initFields = {
+  user: null,
+  recipes: []
+}
 
 class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = {user:null};
+    this.state = {... initFields};
   }
   componentDidMount() {
     const data = JSON.parse(sessionStorage.getItem('userData'));
     let user=data;
     console.log(user);
     this.setState({user: user});
+
+    var email = this.getEmail();
+    this.grabRecipes(email);
+
   }
+
+  grabRecipes = (email) => {
+    this.props.firebase.db.collection("recipes").get().then(this.onResult);
+  }
+
+  onResult = (querySnapshot) => {
+   var recipes = [];
+   var search = this.getEmail();
+   console.log(search)
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+            if(doc.data().user.toLowerCase().includes(search)){
+                console.log(doc.id, " => ", doc.data().user);
+                recipes.push(doc.id);
+                console.log(recipes)
+            }
+        });
+    console.log(recipeTitles);
+    this.setState({recipes: recipes});
+  } 
+
   isSignedIn() {
     return (this.state.user !== null)
   }
@@ -47,11 +78,16 @@ class Home extends Component {
     }
   }
   render(){
-
+    var recipeCards = this.state.recipes.map((id)=><PreviewCard id={id}/>)
     return(
-  <div class="container-fluid">
-    <div class="row"><h1 class="s-4 animate__animated animate__fadeInLeft">Hello {this.getFirstName()}</h1></div>
-  </div>);
+      <div class="container-fluid">
+        <div class="row"><h1 class="s-4 animate__animated animate__fadeInLeft">Hello {this.getFirstName()}</h1></div>
+            <div class="d-flex justify-content-center">
+                <div class="col-3">
+                  {recipeCards}
+                </div> 
+            </div>
+        </div>);
   }
 }
 class userData extends Component {
@@ -100,6 +136,11 @@ class userData extends Component {
       return "no Email";
     }
   }
+
+
+
+
+
   render(){
 
     return;
