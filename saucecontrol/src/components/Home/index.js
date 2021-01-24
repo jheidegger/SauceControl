4a421b32e2dd362d1b2dcdb3f5cac045af7d6dc4
krkral <kralk@purdue.edu>
2021-01-24 05:48:43 -0500

    more defined home message

--- saucecontrol/src/components/Home/index.js
+++ saucecontrol/src/components/Home/index.js
@@ -49,9 +49,10 @@ class Home extends Component {
   render(){
 
     return(
-  <div class="container-fluid">
-    <div class="row"><h1 class="s-4 animate__animated animate__fadeInLeft">Hello {this.getFirstName()}</h1></div>
-  </div>);
+      <div class="container-fluid">
+        <div class="row"><h1 class="s-4 animate__animated animate__fadeInLeft">Hello {this.getFirstName()} let's check out your recipes!</h1></div>
+      </div>
+    );
   }
 }
 class userData extends Component {
